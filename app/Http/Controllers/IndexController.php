a3f2124c6695b5a81876cc0ee2099c50f2bc59b5
ramon <ramon@cherryline.ru>
2024-06-05 20:36:06 +0300

    feat: start new project + files

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Services\CmsRouter;
+use Illuminate\Support\Facades\Auth;
+
+class IndexController extends Controller
+{
+
+
+    public function index(){
+        return view('welcome');
+    }
+
+    public function getRouter(Request $request, CmsRouter $cmsRouter)
+    {
+        // добавить проверку авторизации когда будет каркас авторизации
+//        if(!Auth::check()){
+//            return [];
+//        }else{
+//            return $cmsRouter->getRoutes(Auth::user());
+//        }
+
+
+
+        return $cmsRouter->getRoutes();
+    }
+}
